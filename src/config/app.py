43ff3dd43373146c9c0efe2e1e610ede09451d22
Joao.Nogueira <jprnogueira@yahoo.com.br>
2025-03-23 07:23:34 -0300

    chore: force deploy

--- src/config/app.py
+++ src/config/app.py
@@ -1,6 +1,9 @@
 from fastapi import FastAPI
 
+from src.entrypoints import router
+
 
 def create_app():
     app = FastAPI()
+    router.add_routes(app)
     return app
--- src/main.py
+++ src/main.py
@@ -1,6 +1,3 @@
 from src.config.app import create_app
-from src.entrypoints import router
 
 app = create_app()
-
-router.add_routes(app)
