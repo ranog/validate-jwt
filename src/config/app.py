c95f2bbf407357275cb000e2d8ad4793b29bf6db
Joao.Nogueira <jprnogueira@yahoo.com.br>
2025-03-23 05:18:17 -0300

    chore: fix for alb to recognize as healthy

--- src/config/app.py
+++ src/config/app.py
@@ -1,9 +1,6 @@
 from fastapi import FastAPI
 
-from src.entrypoints import router
-
 
 def create_app():
     app = FastAPI()
-    router.add_routes(app)
     return app
--- src/main.py
+++ src/main.py
@@ -1,3 +1,6 @@
 from src.config.app import create_app
+from src.entrypoints import router
 
 app = create_app()
+
+router.add_routes(app)
