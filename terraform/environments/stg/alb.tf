3838c18b4520944213167c843e4fa9bb8f1e99aa
Joao.Nogueira <jprnogueira@yahoo.com.br>
2025-03-24 20:44:15 -0300

    chore: add create_before_destroy

--- terraform/environments/stg/alb.tf
+++ terraform/environments/stg/alb.tf
@@ -33,6 +33,10 @@ resource "aws_lb_target_group" "app" {
   vpc_id   = aws_vpc.main.id
   target_type = "ip"
 
+  lifecycle {
+    create_before_destroy = true
+  }
+
   health_check {
     path = "/health"
     port = "traffic-port"
