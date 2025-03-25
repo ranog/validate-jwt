f98165753c87e1f06ae7bc93188fab4c1e95c813
Joao.Nogueira <jprnogueira@yahoo.com.br>
2025-03-25 20:24:28 -0300

    chore: fix to deploy images to the cloud

--- terraform/environments/stg/variables.tf
+++ terraform/environments/stg/variables.tf
@@ -12,3 +12,8 @@ variable "vpc_cidr_block" {
   type        = string
   default     = "10.0.0.0/16"
 }
+
+variable "image_tag" {
+  type        = string
+  description = "Tag da imagem Docker a ser usada no ECS (ex: GITHUB_SHA)"
+}
