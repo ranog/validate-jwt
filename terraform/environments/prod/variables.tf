ca2f55229056618c3c86c736a930ff1cc25796d3
Joao.Nogueira <jprnogueira@yahoo.com.br>
2025-03-26 20:37:16 -0300

    chore: fix deploy

--- terraform/environments/prod/variables.tf
+++ terraform/environments/prod/variables.tf
@@ -1,19 +1,19 @@
-# variable "app_name" {
-#   type        = string
-#   description = "Nome da aplicação"
-# }
-#
-# variable "container_port" {
-#   type        = number
-#   default     = 8080
-# }
-#
-# variable "vpc_cidr_block" {
-#   type        = string
-#   default     = "10.0.0.0/16"
-# }
-#
-# variable "image_tag" {
-#   type        = string
-#   description = "Tag da imagem Docker a ser usada no ECS (ex: GITHUB_SHA)"
-# }
+variable "app_name" {
+  type        = string
+  description = "Nome da aplicação"
+}
+
+variable "container_port" {
+  type        = number
+  default     = 8080
+}
+
+variable "vpc_cidr_block" {
+  type        = string
+  default     = "10.0.0.0/16"
+}
+
+variable "image_tag" {
+  type        = string
+  description = "Tag da imagem Docker a ser usada no ECS (ex: GITHUB_SHA)"
+}
