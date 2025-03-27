3342043c9da0e32b3ff9dfd505109580aa4c7565
Joao.Nogueira <jprnogueira@yahoo.com.br>
2025-03-26 21:10:14 -0300

    chore: remove the rest of the infrastructure

--- terraform/environments/stg/ecs.tf
+++ terraform/environments/stg/ecs.tf
@@ -1,57 +1,57 @@
-resource "aws_iam_role" "ecs_task_execution" {
-  name = "${var.app_name}-execution-role"
+# resource "aws_iam_role" "ecs_task_execution" {
+#   name = "${var.app_name}-execution-role"
+#
+#   assume_role_policy = jsonencode({
+#     Version = "2012-10-17"
+#     Statement = [{
+#       Action    = "sts:AssumeRole"
+#       Effect    = "Allow"
+#       Principal = {
+#         Service = "ecs-tasks.amazonaws.com"
+#       }
+#     }]
+#   })
+# }
+#
+# resource "aws_iam_role_policy_attachment" "ecs_execution" {
+#   role       = aws_iam_role.ecs_task_execution.name
+#   policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
+# }
+#
+# resource "aws_ecs_cluster" "main" {
+#   name = "${var.app_name}-cluster"
+# }
 
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [{
-      Action    = "sts:AssumeRole"
-      Effect    = "Allow"
-      Principal = {
-        Service = "ecs-tasks.amazonaws.com"
-      }
-    }]
-  })
-}
-
-resource "aws_iam_role_policy_attachment" "ecs_execution" {
-  role       = aws_iam_role.ecs_task_execution.name
-  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
-}
-
-resource "aws_ecs_cluster" "main" {
-  name = "${var.app_name}-cluster"
-}
-
-resource "aws_ecs_task_definition" "app" {
-  family                   = var.app_name
-  network_mode             = "awsvpc"
-  requires_compatibilities = ["FARGATE"]
-  cpu                      = "256"
-  memory                   = "512"
-  execution_role_arn       = aws_iam_role.ecs_task_execution.arn
-
-  container_definitions = jsonencode([
-    {
-      name      = var.app_name
-      image = "${aws_ecr_repository.app.repository_url}:${var.image_tag}"
-      essential = true
-      portMappings = [
-        {
-          containerPort = var.container_port
-          protocol      = "tcp"
-        }
-      ]
-      logConfiguration = {
-        logDriver = "awslogs"
-        options = {
-          awslogs-group         = "/ecs/${var.app_name}"
-          awslogs-region        = "us-west-2"
-          awslogs-stream-prefix = "ecs"
-        }
-      }
-    }
-  ])
-}
+# resource "aws_ecs_task_definition" "app" {
+#   family                   = var.app_name
+#   network_mode             = "awsvpc"
+#   requires_compatibilities = ["FARGATE"]
+#   cpu                      = "256"
+#   memory                   = "512"
+#   execution_role_arn       = aws_iam_role.ecs_task_execution.arn
+#
+#   container_definitions = jsonencode([
+#     {
+#       name      = var.app_name
+#       image = "${aws_ecr_repository.app.repository_url}:${var.image_tag}"
+#       essential = true
+#       portMappings = [
+#         {
+#           containerPort = var.container_port
+#           protocol      = "tcp"
+#         }
+#       ]
+#       logConfiguration = {
+#         logDriver = "awslogs"
+#         options = {
+#           awslogs-group         = "/ecs/${var.app_name}"
+#           awslogs-region        = "us-west-2"
+#           awslogs-stream-prefix = "ecs"
+#         }
+#       }
+#     }
+#   ])
+# }
 
 # resource "aws_security_group" "ecs" {
 #   name   = "${var.app_name}-ecs-sg"
